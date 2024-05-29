f5465b6be4151e7832a5c0fd6aae8242ee1b54b1
gloriaromeroorobio <163323438+gloriaromeroorobio@users.noreply.github.com>
2024-05-29 17:34:00 +0200

    Update streamChatGPT.py

--- streamChatGPT.py
+++ streamChatGPT.py
@@ -1,12 +1,13 @@
 from openai import OpenAI
 import streamlit as st
 
-st.title("ChatGPT-like clone")
+st.title("ChatGPT-like clone Glori")
 
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+    #st.session_state["openai_model"] = "gpt-3.5-turbo"
+    st.session_state["openai_model"] = "gpt-4"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
